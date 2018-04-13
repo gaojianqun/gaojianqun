be1882b087eb08a7e4e11e8da6d7741c7bbaf0a0
600219 <gaojianqun@syswin.com>
2018-04-13 11:45:20 +0800

    SpringCloud全套体系

---
+++ springCloud/gateway-service/src/main/java/com/example/demo/impl/HelloServiceImplHystrixFallBack.java
@@ -0,0 +1,23 @@
+package com.example.demo.impl;
+
+import com.example.demo.HelloService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by gaojianqun on 2018/3/15.
+ * 服务降级
+ */
+
+@Component
+public class HelloServiceImplHystrixFallBack implements HelloService{
+    @Override
+    public String hello() {
+        return "no hello world";
+    }
+
+    @Override
+    public String hello1(@RequestParam( value="name") String name) {
+        return "error";
+    }
+}
