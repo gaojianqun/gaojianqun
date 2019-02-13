d258de0b432454a57acc91cfad3b8a65e562f856
gaojianqun <gaojianqun@syswin.com>
2019-02-13 11:11:03 +0800

    布隆过滤器测试

--- DesignPattern/Spring/src/com/spring/design/pattern/single/Singleton5.java
+++ DesignPattern/Spring/src/com/spring/design/pattern/single/Singleton5.java
@@ -2,15 +2,23 @@ package com.spring.design.pattern.single;
 
 /**
  * Created by gaojianqun on 2018/12/16.
+ * 静态内部类实现
  */
 public class Singleton5 {
 
-    private static class SingletonHolder{
-        public final static Singleton5 instance = new Singleton5();
+    //私有化构造器
+    private Singleton5(){
+
     }
 
+    //对外提供公共的访问方法
     public static Singleton5 getInstance(){
         return SingletonHolder.instance;
+    }
+
+    //静态内部类，里面实例化外部类
+    private static class SingletonHolder{
+        public final static Singleton5 instance = new Singleton5();
     }
 
 }
