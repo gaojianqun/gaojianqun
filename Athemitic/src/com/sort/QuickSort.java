525a556dea6f760a4506217521421ada18a959ee
gaojianqun <gaojianqun@syswin.com>
2019-03-04 16:31:30 +0800

    快速排序优化

--- Athemitic/src/com/sort/QuickSort.java
+++ Athemitic/src/com/sort/QuickSort.java
@@ -3,7 +3,7 @@ package com.sort;
 public class QuickSort {
 
     public static void main(String [] args){
-        int [] array = {2,10,5,8,3,6,1,4,9,7};
+        int [] array = {6,1,2,7,9,3,4,5,10,8};
         int low = 0,high = array.length - 1;
         sort(array,low,high);
 
@@ -15,36 +15,39 @@ public class QuickSort {
 
     public static void sort(int[] array,int low,int high){
        int start = low,end = high;
-       int key = array[start];
        while(start < end){
+
+           int key = array[start];
+
            while (key <= array[end] && end > start){
-               end --;
+               end--;
            }
 
-           if(key >= array[end]){
+           if(array[end] <= key){
                int temp = array[end];
                array[end] = array[start];
                array[start] = temp;
            }
 
-           while(key >= array[start] && start < end){
-               start ++;
-           }
+            while (key >= array[start] && start < end){
+                start ++;
+            }
 
-           if(key <= array[start]){
-               int temp = array[start];
-               array[start] = array[end];
-               array[end] = temp;
-           }
-
-           if(start > low){
-               sort(array,low,(start-1));
-           }
-
-           if(end < high){
-               sort(array,(end+1),high);
-           }
+            if(array[start] >= key){
+                int temp = array[start];
+                array[start] = array[end];
+                array[end] = temp;
+            }
        }
+
+
+        if(start > low){
+            sort(array,low,(start-1));
+        }
+
+        if(end < high){
+            sort(array,(end+1),high);
+        }
 
     }
 
